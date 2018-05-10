9a094708104fd298807cd078a447a7467e57e6f0
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-10 14:39:21 -0700

    game instance unit tests

---
+++ test/game_test.js
@@ -0,0 +1,62 @@
+const expect = require('chai').expect;
+
+describe('GAME INSTANCE FUNCTIONS', function () {
+	describe('checkGameStatus', function () {
+		const checkGameStatus = require('../game_logic/game_instance.js').checkGameStatus;
+		it('should tell me when the game is over', function () {
+			const players = [
+				{
+					ships: [
+						{
+							locations: [[0, 0]],
+							damage: [[0, 0]]
+						}
+					]
+				}
+			];
+			const actual = checkGameStatus(players);
+			expect(actual).to.be.false;
+		});
+	});
+
+	describe('takeTurn', function () {
+		const takeTurn = require('../game_logic/game_instance').takeTurn;
+		let guess, player;
+
+		beforeEach(function () {
+			guess = function () { return [0, 0]; };
+			player = {
+				ships: [
+					{
+						locations: [[0, 0]],
+						damage: []
+					}
+				]
+			};
+		});
+
+		it('should return false if the game ends', function () {
+			const actual = takeTurn(player, guess);
+			expect(actual).to.be.false;
+		});
+	});
+
+	function saveGame (callback) {
+		setTimeout(function () {
+			callback();
+		}, 1000);
+	}
+
+	describe('saveGame', function () {
+		it('should update save status', function (done) {
+			let status = 'game not saved...';
+
+			saveGame(function () {
+				status = 'game saved!';
+				expect(status).to.equal('game saved!');
+				done();
+			});
+		});
+	});
+
+});
