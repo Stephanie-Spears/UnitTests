b109a787b47ac6f03de04fc7697f148db818ff30
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-10 09:31:26 -0700

    additional method tests

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -5,13 +5,11 @@ function checkForShip(player, coordinates) {
 		shipPresent = ship.locations.filter(function (actualCoordinate) {
 			return(actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1]);
 		})[0];
-		if (!shipPresent){
-			return false;
-		} else{
+		if (shipPresent) {
 			return true;
 		}
 	}
-
+	return false;
 }
 
 module.exports.checkForShip = checkForShip;
